2634d9c8ef25c4e1198824423c36c8f0b0f51d20
thisdk <thisdk@gmail.com>
2022-07-08 22:57:46 +0800

    bump version 1.0.0

--- app/src/main/java/io/github/thisdk/camera/MainFragment.kt
+++ app/src/main/java/io/github/thisdk/camera/MainFragment.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.github.niqdev.mjpeg.DisplayMode
+import com.github.niqdev.mjpeg.Mjpeg
+import com.github.niqdev.mjpeg.MjpegInputStream
 import io.github.thisdk.camera.databinding.FragmentMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+
+    private var first = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +32,42 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.start.setOnClickListener {
+            playCameraStream()
+        }
+        lifecycleScope.launch {
+            delay(3000)
+            binding.start.isEnabled = true
+        }
+    }
+
+    private fun playCameraStream() {
+        binding.start.isEnabled = false
+        Mjpeg.newInstance()
+            .open("http://127.0.0.1:53866/?action=stream", 15)
+            .subscribe(
+                { stream: MjpegInputStream ->
+                    binding.video.setSource(stream)
+                    binding.video.setDisplayMode(DisplayMode.BEST_FIT)
+                    binding.video.showFps(false)
+                    binding.start.isEnabled = true
+                    Toast.makeText(context, "内网穿透成功!", Toast.LENGTH_SHORT).show()
+                }
+            ) { throwable: Throwable? ->
+                Toast.makeText(context, throwable?.message, Toast.LENGTH_SHORT).show()
+                binding.start.isEnabled = true
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!first) playCameraStream()
+        first = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.video.stopPlayback()
     }
 
     override fun onDestroyView() {
